ccd8b5148c8fcaf66fc2bce5ef6abb7d6f314b3d
fiinasikapauu <2311523041_rafi@gmail.com>
2025-06-11 16:03:45 +0700

    sec

--- SIMTASI/app.js
+++ SIMTASI/app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,18 +20,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
+// Konfigurasi session
+app.use(session({
+  secret: 'rahasiaTA',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Router utama
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', authRouter); // Gunakan router untuk auth (signup & signin)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -39,3 +50,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
